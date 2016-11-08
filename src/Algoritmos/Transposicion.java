6ba2b8effc05a870d55d296f5452e8b013768e50
Andromeda <josh.an.bas@gmail.com>
2016-11-08 10:32:19 -0600

    Changes message

--- src/Algoritmos/Transposicion.java
+++ src/Algoritmos/Transposicion.java
@@ -5,24 +5,26 @@
  */
 package Algoritmos;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 import javax.swing.JOptionPane;
 
 /**
  *
  * @author Josh <github:Josh2604>
  */
+    /***********************************************************
+     *              Algoritmo de cifrado por transposicion     *
+     *                by(github.com/Josh2604)                  *
+     *                                                         *
+     ***********************************************************/
 public class Transposicion extends javax.swing.JFrame {
 
     private static int TAM = 0;
-    private static char ELEMENTOS_CIFRADO[] = {
+    private static final char ELEMENTOS_CIFRADO[] = {
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
         '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
         '`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '=', '+', '/', '.', ',', '?', '|','\'','"','?',';',
-        ':','<','>','{','}','[',']','_'};
+        ':','<','>','{','}','[',']','_',' '};
 
     /**
      * Creates new form Transposicion
